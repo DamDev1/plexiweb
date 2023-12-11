9c350087bb4254c11089b803d2d3762aab4a9dd7
DamDev1 <samueldamilare622@gmail.com>
2023-12-11 04:13:07 -0800

    first commit

--- projects/eatsy/js/main.js
+++
@@ -1,8 +0,0 @@
-var Scrollbar = window.Scrollbar;
-
-Scrollbar.init(document.querySelector('body'));
-
-const toggle = document.querySelector(".toggle").addEventListener("click", () =>{
-    document.querySelector(".nav-menu").classList.toggle("active-nav")
-  })
-  
