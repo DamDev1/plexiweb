38c078b421baa5e2e6abb9bf33e950d0dcf7fa63
Gideon <125488605+GideonJulian@users.noreply.github.com>
2024-01-09 19:18:49 +0100

    Gideon Added

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,19 +1,21 @@
-import React from 'react'
-import { Link } from 'react-router-dom'
+import React from "react";
+import { Link } from "react-router-dom";
 
 export default function Home() {
   return (
     <main>
-      <section id='hero-section'>
+      <section id="hero-section">
         <div className="container-col">
           <div className="col-1">
             <h1>YOUR BRAND IMAGE MATTERS</h1>
             <div className="hero-content">
               <div className="content">
-                <p>Plexiweb  is a strategy-led design studio specializing in brand refreshes. We help
-                  service-based solopreneurs & small business
-                  owners build a standout brand image — one
-                  they can be proud of.</p>
+                <p>
+                  Plexiweb is a strategy-led design studio specializing in brand
+                  refreshes. We help service-based solopreneurs & small business
+                  owners build a standout brand image — one they can be proud
+                  of.
+                </p>
                 <Link>explore our work</Link>
               </div>
             </div>
@@ -25,8 +27,7 @@ export default function Home() {
         </div>
       </section>
 
-
-      <section id='showcase'>
+      <section id="showcase">
         <div className="showcase-container">
           <div className="showcase-conpanies">
             <span>
@@ -54,7 +55,11 @@ export default function Home() {
               <h3>tired of staring at your</h3>
             </div>
             <div className="right">
-              <p>An old and boring brand identity and website can leave your customers feeling confused, uncertain, and searching for something better. We're here to help you change that.</p>
+              <p>
+                An old and boring brand identity and website can leave your
+                customers feeling confused, uncertain, and searching for
+                something better. We're here to help you change that.
+              </p>
 
               <div className="more-services">
                 <div className="services-content">
@@ -66,8 +71,14 @@ export default function Home() {
                     </div>
                   </div>
                   <div className="message">
-                      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsam laboriosam, nobis optio autem voluptatibus blanditiis odio, nihil tempore nemo, quibusdam modi at laudantium cum animi nesciunt saepe deleniti? Sequi, doloremque.</p>
-                    </div>
+                    <p>
+                      Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                      Ipsam laboriosam, nobis optio autem voluptatibus
+                      blanditiis odio, nihil tempore nemo, quibusdam modi at
+                      laudantium cum animi nesciunt saepe deleniti? Sequi,
+                      doloremque.
+                    </p>
+                  </div>
                 </div>
 
                 <div className="services-content">
@@ -79,8 +90,14 @@ export default function Home() {
                     </div>
                   </div>
                   <div className="message">
-                      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsam laboriosam, nobis optio autem voluptatibus blanditiis odio, nihil tempore nemo, quibusdam modi at laudantium cum animi nesciunt saepe deleniti? Sequi, doloremque.</p>
-                    </div>
+                    <p>
+                      Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                      Ipsam laboriosam, nobis optio autem voluptatibus
+                      blanditiis odio, nihil tempore nemo, quibusdam modi at
+                      laudantium cum animi nesciunt saepe deleniti? Sequi,
+                      doloremque.
+                    </p>
+                  </div>
                 </div>
 
                 <div className="services-content">
@@ -92,8 +109,14 @@ export default function Home() {
                     </div>
                   </div>
                   <div className="message">
-                      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsam laboriosam, nobis optio autem voluptatibus blanditiis odio, nihil tempore nemo, quibusdam modi at laudantium cum animi nesciunt saepe deleniti? Sequi, doloremque.</p>
-                    </div>
+                    <p>
+                      Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                      Ipsam laboriosam, nobis optio autem voluptatibus
+                      blanditiis odio, nihil tempore nemo, quibusdam modi at
+                      laudantium cum animi nesciunt saepe deleniti? Sequi,
+                      doloremque.
+                    </p>
+                  </div>
                 </div>
                 <div className="view-service">
                   <Link>VIEW OUR SERVICES</Link>
@@ -102,7 +125,45 @@ export default function Home() {
             </div>
           </div>
         </div>
+        {/* Work we proud of */}
+        <div id="work-container">
+          <div className="text-box">
+            <h1>WORK WE'RE PROUD OF</h1>
+            <p>
+              100+ businesses and non-profits have trusted us with their brand.
+              You can too.
+            </p>
+          </div>
+          <div className="view-work">
+            <Link>View all work</Link>
+          </div>
+          <div className="work-col-1">
+            <div className="left">
+              <p>BRAND REFRESH</p>
+              <h3>Enamel Dentistry</h3>
+              <p>Local / Service Business</p>
+            </div>
+
+            <div className="right"></div>
+          </div>
+        </div>
+        <div className="projects-container">
+          <div className="project">
+            <div className="img-card"></div>
+            <div className="project-desc">
+              <p>BRAND REFRESH</p>
+              <h2>Philip Brideaux</h2>
+            </div>
+          </div>
+          <div className="project">
+            <div className="img-card"></div>
+            <div className="project-desc">
+              <p>BRAND REFRESH</p>
+              <h2>NGMarketing</h2>
+            </div>
+          </div>
+        </div>
       </section>
     </main>
-  )
+  );
 }
