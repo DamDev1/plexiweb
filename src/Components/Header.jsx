57e1b0e8a90501ac297bfed7f67e22c42fa63323
DamDev1 <agbelemosetonji@gmail.com>
2024-01-08 17:26:49 -0800

    creating the hero section

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -1,7 +1,26 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 
 export default function Header() {
   return (
-    <div>Header</div>
+    <header>
+      <div className="container">
+        <div className="logo">
+          <Link>Plexiweb Studio.</Link>
+        </div>
+
+        <nav>
+          <div className="nav-links">
+            <Link>WORK</Link>
+            <Link>SERVICES</Link>
+            <Link>CULTURE</Link>
+            <Link>JOURNAL</Link>
+          </div>
+        </nav>
+        <div className="contact">
+            <Link>CONTACT</Link>
+          </div>
+      </div>
+    </header>
   )
 }
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,7 +1,29 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 
 export default function Home() {
   return (
-    <div>Home</div>
+    <main>
+      <section id='hero-section'>
+        <div className="container-col">
+          <div className="col-1">
+            <h1>YOUR BRAND IMAGE MATTERS</h1>
+            <div className="hero-content">
+              <div className="content">
+                <p>Plexiweb  is a strategy-led design studio specializing in brand refreshes. We help
+                  service-based solopreneurs & small business
+                  owners build a standout brand image — one
+                  they can be proud of.</p>
+                <Link>explore our work</Link>
+              </div>
+            </div>
+          </div>
+          <div className="hero-image">
+            <div className="image"></div>
+            <span>Khu·la / Kh·ula</span>
+          </div>
+        </div>
+      </section>
+    </main>
   )
 }
